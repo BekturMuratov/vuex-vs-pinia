9976977400912a411444cd7b6d5d41a6b11e9ccb
BekturMuratov <Muratov225@gmail.com>
2022-08-10 17:58:24 +0600

    vuex vs pinia using vue and axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    users: []
+  },
+  getters: {
+    getUsers: (state) => state.users 
   },
   mutations: {
+    SET_USERS(state,users) {
+      state.users = users
+    }
   },
   actions: {
+    async fetchUsers({ commit }) {
+      try {
+        const data = await axios.get('https://jsonplaceholder.typicode.com/users')
+          commit('SET_USERS', data.data)
+        }
+        catch (error) {
+            alert(error)
+            console.log(error)
+        }
+    }
   },
   modules: {
   }
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>Made By Getters</h1>
+  <div v-for='gettersUser in gettersUsers' :key='gettersUser.id'>
+    {{gettersUser.id}} {{gettersUser.name}} {{gettersUser.address}}
+    </div>
+    <h1>Made By Actions</h1>
+  <div v-for='user in users' :key='user.id'>
+    {{user.id}} {{user.name}} {{user.address}}
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted, computed } from 'vue';
+//import the global store object from Vuex
+import {useStore} from 'vuex'
+// declare the store variable
+const store = useStore();
+
+const msg = ref("Welcome to my Vuex Store");
+
+const getUsers = computed(() => {
+  return store.getters.getUsers
+})
+
+const users = computed(() => {
+  return store.state.users
+})
+
+onMounted(() => {
+// dispatch the fetchUsers action which commits a mutation to update the state
+  store.dispatch("fetchUsers");
+})
+</script>
